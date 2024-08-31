394e0a91a99f2a363c835de282db888d833fb6a4
Geuni <dlrmsgnl620@naver.com>
2024-08-31 23:27:11 +0900

    feat: css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import { useState } from 'react';
-
-
-
-import { Button } from 'src/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export const App = () => {
-
-
-  
   return (
-    <div className="absolute left-10 top-0">
-      <Button>Click me</Button>
-    </div>
+    <main className="w-screen h-screen flex justify-center items-center">
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="title">
+              <span className="text">
+                제목제목제목제목제목제목제목제목제목제목제목제목 제목제목제목
+              </span>
+            </DialogTitle>
+            <DialogDescription>내용내용</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </main>
   );
 };
