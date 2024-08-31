830ea1a2f537cfce7f9b5e671b0aa20d024d662f
Geuni <dlrmsgnl620@naver.com>
2024-08-31 16:18:16 +0900

    migrate lint to biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+
+
+
 import { Button } from 'src/components/ui/button';
 
 export const App = () => {
+
+
+  
   return (
     <div className="absolute left-10 top-0">
       <Button>Click me</Button>
